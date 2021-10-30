84c508259229e49fd7ccbdcdfd28c480f6d971e8
Aditya Byreddy <mr.adithyan123@gmail.com>
2021-10-30 19:34:03 +0530

    Fix mis-spelled string

--- NumbersSubtraction.java
+++ NumbersSubtraction.java
@@ -1,5 +1,5 @@
 public class NumbersSubtraction {
-    public static void main(string args[]){
+    public static void main(String args[]){
         Integer a = 20, b = 10;
         System.out.println(a-b);
     }
